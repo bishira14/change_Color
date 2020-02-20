d37cc7ac9a14602885a590d483315a00d5067801
Hamza Bishira <bishira12@hotmail.co.uk>
2020-02-20 15:20:58 +0000

    latest update

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ let theButton = document.querySelector("button");
 let theBackground = document.querySelector("body");
 let colors = ["yellow", "lightblue", "orange", "purple", "green"]
 theButton.addEventListener("click", function(){
-  theBackground.style.backgroundColor = colors[0];
+  let random = Math.floor(Math.random() * colors.length);
+  console.log(random);
+  theBackground.style.backgroundColor = colors[random];
 })
